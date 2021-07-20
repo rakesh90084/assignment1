23691dafe128e6ced8778d1f0570a7b4df57022d
rakesh90084 <90084raky@gmail.com>
2021-07-20 22:25:15 +0530

    employee

---
+++ emp.py
@@ -0,0 +1,19 @@
+employees=["Ram","Sham","Sita","Gita"]
+print(employees)
+del employees[3]
+print(employees)
+employees.remove("Sham")
+print (employees)
+employees.pop(1)
+print (employees)
+employees.clear
+print(employees)
+employees=["Ram","Sham","Sita","Gita"]
+print (employees)
+employees[0]="rahul"
+employees[1]="rakesh"
+print(employees)
+print(len(employees))
+empid=[100,200,300]
+print(employees+empid)
+print(empid*4)
